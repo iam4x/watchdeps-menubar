4f1e5fdf1651c472c707116438612b258348eed2
iam4x <iam4x@me.com>
2015-10-24 04:57:28 +0200

    style: add simple header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <header className='app--header'>
+        <div className='container-fluid'>
+          WatchDeps
+        </div>
+      </header>
+    );
+  }
+
+}
+
+export default Header;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from 'components/Header';
 
 export default function Layout({ children }) {
   return (
-    <div className='app--content'>
-      { children }
+    <div className='app--wrapper'>
+      <Header />
+      <div className='app--content'>
+        { children }
+      </div>
     </div>
   );
 }
